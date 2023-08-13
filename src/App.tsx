9de9539ca77d590ac3903a5e289f5ee1747b191c
Serge Kovbasiuk <isserge@protonmail.com>
2023-08-13 12:48:00 +0700

    remove router, prompt route and chat button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import { Header, Notification } from './components';
 import { Main } from './Main';
-import { Prompt } from './Prompt';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
-        <Notification />
-        <Header />
-        <main className="p-4">
-          <div className="container max-w-full">
-            <Routes>
-              <Route path="/prompt" element={<Prompt />} />
-              <Route path="/" element={<Main />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Notification />
+      <Header />
+      <main className="p-4">
+        <div className="container max-w-full">
+          <Main />
+        </div>
+      </main>
+    </div>
   );
 }
 
